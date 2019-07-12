dbe813af35f6989627eed5528bde7ffb2f1d727f
icecream <zanjarwhite@gmail.com>
2019-07-13 00:18:53 +0600

    'models design Propertyfor sell, but google map location selection not adding'

--- properties/models.py
+++ properties/models.py
@@ -20,13 +20,31 @@ class Type(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class PropertyForSell(models.Model):
-    realator=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,default=None)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    type=models.ForeignKey(Type,on_delete=models.CASCADE)
+    STATUS_CHOICES=(
+        ('draf','Draft'),
+        ('published','Published')
+    )
+    realator=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING,default=None)
+    category=models.ForeignKey(Category,on_delete=models.DO_NOTHING)
+    type=models.ForeignKey(Type,on_delete=models.DO_NOTHING)
     title=models.CharField(max_length=200)
+    full_description=models.TextField()
+    key_features=models.TextField()
+    min_price=models.IntegerField()
+    max_price=models.IntegerField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    slug=models.SlugField()
+    saved=models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,related_name='saved')
+    status=models.CharField(max_length=12,choices=STATUS_CHOICES,default='published')
+    published=PublishedManager() #Costom model manager
+
+
 
 
     def __str__(self):
