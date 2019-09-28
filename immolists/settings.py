e4897b2dfb372ba372f52b8fa24bcb60e95591be
rahi <zanjarwhite@gmail.com>
2019-09-28 16:10:19 +0600

    postgs done

--- immolists/settings.py
+++ immolists/settings.py
@@ -119,8 +119,12 @@ ACCOUNT_FORMS = {'login': 'accounts.forms.MyLoginForm','signup':'accounts.forms.
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'immolists_db',
+        'USER': 'rahi',
+        'PASSWORD': 'rahi',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
